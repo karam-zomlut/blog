6def87b23e973eb7e7cb61f5556b2137b3a0fe28
karam-zomlut <zomlutk@gmail.com>
2023-03-10 22:53:16 +0200

    Delete images route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,11 @@ import express, { Request, Response } from 'express';
 import userRouter from './users';
 import postRouter from './posts';
 import { serverError } from '../controllers';
-import imageRouter from './images';
 
 const router = express.Router();
 
 router.use('/users', userRouter);
 router.use('/posts', postRouter);
-router.use('/images', imageRouter);
 router.use(serverError);
 
 export default router;
