2f0e8f3db6c878a541de4f1f334667caf2b12230
karam-zomlut <zomlutk@gmail.com>
2023-03-10 22:55:01 +0200

    add the image to query when create the post

--- src/database/query/posts/createPostQuery.ts
+++ src/database/query/posts/createPostQuery.ts
@@ -5,12 +5,13 @@ type Post = {
   content: string;
   slug: string;
   userId: number;
+  image: string;
 };
 
-const createPostQuery = async ({ title, content, slug, userId }: Post) => {
+const createPostQuery = async ({ title, content, slug, image, userId }: Post) => {
   const sql = {
-    text: `INSERT INTO posts (title, content, slug, user_id) VALUES ($1, $2, $3, $4) RETURNING *`,
-    values: [title, content, slug, userId],
+    text: `INSERT INTO posts (title, content, slug, image, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    values: [title, content, slug, image, userId],
   };
 
   return connection.query(sql);
