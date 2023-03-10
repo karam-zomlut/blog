fee7aa06c9e97444b7392fb3d3ba540b47e461d2
karam-zomlut <zomlutk@gmail.com>
2023-03-10 20:00:39 +0200

    Setup multer to upload files

--- src/utils/multer/upload.ts
+++ src/utils/multer/upload.ts
@@ -5,10 +5,18 @@ const storage = multer.diskStorage({
     cb(null, './uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
+    // remove spaces from original file name
+    cb(
+      null,
+      file.fieldname +
+        '-' +
+        Date.now() +
+        '-' +
+        file.originalname.replace(/\s/g, '')
+    );
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 export default upload;
